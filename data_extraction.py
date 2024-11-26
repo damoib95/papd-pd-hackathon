38275e64fde77754be87cb59dc92bca3a5711049
damoib95 <damoib95@gmail.com>
2024-11-25 23:11:29 -0600

    extracción de texto desde vídeo

---
+++ data_extraction.py
@@ -0,0 +1,35 @@
+import os
+import logging
+from pydub import AudioSegment
+import whisper
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+
+def video_to_text():
+    filename = os.getenv("FILENAME")
+    format = os.getenv("INPUT_TYPE")
+    video_path = os.path.join('data', 'input', f'{filename}.{format}')
+    logging.info(f'Abriendo archivo {video_path}')
+    video = AudioSegment.from_file(video_path, format=format)
+    audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2)
+
+    audio_path = os.path.join('data', 'output', f"{filename}_tmp.wav")
+    logging.info(f'Exportando audio {audio_path}')
+    audio.export(audio_path, format="wav")
+
+    language = os.getenv("LANGUAGE")
+    model = whisper.load_model("base")
+    logging.info('Generando transcripción de audio')
+    result = model.transcribe(audio_path, language=language, fp16=False)
+
+    text_path = os.path.join('data', 'output', f"{filename}_tmp.txt")
+    with open(text_path, "w") as file:
+        file.write(result["text"])
+
+    logging.info('Limpiando archivos temporales')
+    os.remove(audio_path)        
+
+    
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 import logging
+from dotenv import load_dotenv
+from data_extraction import video_to_text
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info('Hello, world!')
+    load_dotenv()
+    format = os.getenv("FORMAT")
+    if format in 'mp4':
+        video_to_text()
+
 
 if __name__ == "__main__":
     main()
